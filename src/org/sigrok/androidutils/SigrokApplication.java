f0d3cf4b0c86b1cd4e0e28122f8167b02fb1acdb
Marcus Comstedt <marcus@mc.pp.se>
2016-12-07 21:24:47 +0100

    Include the device filter XML in the AAR
    Also add a convenience method for creating a UsbSupplicant from it.

--- src/org/sigrok/androidutils/SigrokApplication.java
+++ src/org/sigrok/androidutils/SigrokApplication.java
@@ -29,6 +29,8 @@ public class SigrokApplication extends Application {
 
 	private static final String JNI_LIBS_RESOURCE_ID_META =
 	    "org.sigrok.androidutils.jni_libs_resource_id";
+	private static final String DEVICE_FILTER_RESOURCE_ID_META =
+	    "org.sigrok.androidutils.device_filter_resource_id";
 
 	public SigrokApplication()
 	{
@@ -55,6 +57,24 @@ public class SigrokApplication extends Application {
 				System.loadLibrary(libName);
 			}
 		}
+	}
+
+	public static UsbSupplicant createUsbSupplicant(Context context)
+	{
+		ApplicationInfo appInfo = context.getApplicationInfo();
+		try {
+			appInfo = context.getPackageManager().
+				getApplicationInfo(context.getPackageName(),
+				PackageManager.GET_META_DATA);
+		} catch (PackageManager.NameNotFoundException exc) {
+		}
+		if (appInfo.metaData != null &&
+				appInfo.metaData.containsKey(DEVICE_FILTER_RESOURCE_ID_META)) {
+			int resId = appInfo.metaData.getInt(DEVICE_FILTER_RESOURCE_ID_META);
+			return new UsbSupplicant(context, resId);
+		}
+		Log.e("SigrokApplication", "Can't create UsbSupplicant (resource ID missing)");
+		return null;
 	}
 
 	@Override
